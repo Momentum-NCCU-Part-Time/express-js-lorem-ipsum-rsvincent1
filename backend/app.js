2ad601d3e490e86e8e9c3f65c2a81302353c2ea0
rsvincent <rvincent87@gmail.com>
2024-01-13 23:27:34 -0500

    WIP spicy needed

--- backend/app.js
+++ backend/app.js
@@ -1,20 +1,26 @@
-const express = require('express')
+const express = require("express");
 
-const app = express()
-const cors = require('cors')
-const config = { port: process.env.PORT || 3000 }
+const app = express();
+const cors = require("cors");
+const config = { port: process.env.PORT || 3000 };
 
 // middleware
-app.use(cors())
+app.use(cors());
 
 // your API route(s) here
+app.get("/lorem", (req, res) => {
+  const lorem = {
+    lorem:
+      "Dolor esse cupidatat ipsum commodo ut duis id esse voluptate. Consectetur cupidatat non qui sint amet minim aute aliquip.",
+  };
+  res.send(lorem);
+});
 
-
-app.get('*', function (req, res) {
-  res.status(404).json({ error: 'route not found' })
-})
+app.get("*", function (req, res) {
+  res.status(404).json({ error: "route not found" });
+});
 
 // start server
 app.listen(config.port, () => {
-  console.log(`App listening at http://localhost:${config.port}`)
-})
+  console.log(`App listening at http://localhost:${config.port}`);
+});
